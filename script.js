45c7ccf0edc1db2961d7648ffe54beb3c24c1b84
gloria-arriola <43214567+gloria-arriola@users.noreply.github.com>
2019-03-06 08:51:31 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,26 @@
+//Spell #1
+
+$(".transform").click(function(){
+    $(".animal").fadeToggle();
+});
+
+
+
+//Spell #2
+
+$(".book").click(function(){
+    $(".magicBook").toggle();
+});
+
+
+
+//Spell #3
+
+$(".magicwand").click(function(){
+    $(".wand").slideUp();
+    
+ 
+});
+
+
+
